af1f8bd0252b77124e8b4c553a00c2eaf43676d2
BasilMawejje <basil.mawejje@gmail.com>
2019-09-23 19:07:11 +0300

    Able to correctly search API

--- lib/api/v1/app.rb
+++ lib/api/v1/app.rb
@@ -21,6 +21,8 @@ class Product
   validates :price, presence: true
 
   index({ name: 'text' })
+
+  scope :name, -> (name) { where(name: /^#{name}/) }
 end
 
 # Serializers
@@ -51,6 +53,20 @@ namespace '/api/v1' do
     content_type 'application/json'
   end
 
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message:'Invalid JSON' }.to_json
+      end
+    end
+  end
+
   get '/products' do
     products = Product.all
     %i[name description price].each do |filter|
@@ -64,5 +80,16 @@ namespace '/api/v1' do
     product = Product.where(id: id).first
     halt(404, {message: 'Product not found'}.to_json) unless product
     ProductSerializer.new(product).to_json
+  end
+
+  post '/products ' do
+    product = Product.new(json_params)
+    if product.save
+      response.headers['Location'] = "#{base_url}/api/v1/products/#{product.id}"
+      status 201
+    else
+      status 422
+      body ProductSerializer.new(product).to_json
+    end
   end
 end
