e65f024b54a5ecc202a976254d8ed03bc09c0fd3
BasilMawejje <basil.mawejje@gmail.com>
2019-09-27 16:23:21 +0300

    Change how files are included in app.rb

--- lib/api/v1/app.rb
+++ lib/api/v1/app.rb
@@ -1,14 +1,10 @@
 require 'sinatra'
 require 'sinatra/namespace'
 require 'mongoid'
-require './lib/api/v1/helpers/application_helper.rb'
-require './lib/api/v1/controllers/products_controller.rb'
-require './lib/api/v1/controllers/ingredients_controller.rb'
-require './lib/api/v1/models/product.rb'
-require './lib/api/v1/models/ingredient.rb'
-require './lib/api/v1/serializers/product_serializer.rb'
-require './lib/api/v1/serializers/ingredient_serializer.rb'
 require 'sinatra/reloader' if development?
+Dir["./lib/api/v1/**/*.rb"].each { |file|
+  require file
+}
 
 # DB Setup
 configure do
